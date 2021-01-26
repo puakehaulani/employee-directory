67e7b5c8df626d967ea0c9f03f3dbdbc369e13ad
puakehaulani <puakehaulani@gmail.com>
2021-01-26 11:26:07 -0800

    refined api call syntax

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,9 +1,7 @@
+
 import axios from "axios";
-
-function API() {
-  return axios.get("https://randomuser.me/api/20");
-};
-
-export default API
-
-//need to be sure this is going to call and save to one obj that we will sort from, not make a call each filter/sort call made (or else all results will match that parameter and be new random users)
+const BASEURL = "https://randomuser.me/api/?results=20";
+function search() {
+  return axios.get(BASEURL);
+}
+export default search;
